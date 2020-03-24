9402f93c873722e021ee36b7eaa795389beadcb1
unknown <hershdhillon23@gmail.com>
2020-03-24 18:56:53 +0530

    Updates

---
+++ precache-manifest.d6ccbd4724deb0d21fbd43ade366fec7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4309f16826bcbee52ec859ad61f52e4",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "a24ce7e66fb889b1d455",
+    "url": "/portfolio/static/css/2.89f067aa.chunk.css"
+  },
+  {
+    "revision": "ab20c6a90d91980d3580",
+    "url": "/portfolio/static/css/main.f3befcb5.chunk.css"
+  },
+  {
+    "revision": "a24ce7e66fb889b1d455",
+    "url": "/portfolio/static/js/2.96cffed7.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/portfolio/static/js/2.96cffed7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab20c6a90d91980d3580",
+    "url": "/portfolio/static/js/main.6e582160.chunk.js"
+  },
+  {
+    "revision": "f4500de3db46c51f41ef",
+    "url": "/portfolio/static/js/runtime-main.95f49b6d.js"
+  },
+  {
+    "revision": "6a63508a07d601482efcb1bdc6187927",
+    "url": "/portfolio/static/media/devgrub.6a63508a.png"
+  }
+]);
